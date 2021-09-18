f56f8acd41b75d3080347aa7fd581a34301b54a0
Anas-mo101 <77382332+Anas-mo101@users.noreply.github.com>
2021-09-18 15:00:52 +0800

    New improved code

--- test.java
+++ test.java
@@ -2,11 +2,6 @@ import java.util.Scanner;
 
 public class test {
     public static void main (String[] args) { 
-        Scanner input = new Scanner(System.in);
-        System.out.print("ID: ");
-        String User_in = input.nextLine();
-        System.out.print("Password: ");
-        String Pass_in = input.nextLine();
-        User user = new User(User_in,Pass_in);
+        AllMenus Start = new AllMenus();
     }
 }
