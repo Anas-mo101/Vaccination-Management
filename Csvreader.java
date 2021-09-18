a6c5fc38e3db9551dec9b7b010cd09ff02652523
Anas-mo101 <77382332+Anas-mo101@users.noreply.github.com>
2021-09-18 14:55:03 +0800

    New improved code

--- Csvreader.java
+++ Csvreader.java
@@ -1,12 +1,20 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.List;
 
 
 public class Csvreader {
     private List<String> UsersInfo;
     private int UserLocatedInLine;
+    private final int NAME_INDEX = 3;
+    private final int FSTSTATUS_INDEX = 4;
+    private final int SCNDSTATUS_INDEX = 5;     
+    private final int FIRSTVAC_INDEX = 6;      
+    private final int SCNDVAC_INDEX = 7;
+    private final int PHONE_INDEX = 8;
 
     Csvreader(){
         ReadFileLine();
@@ -21,33 +29,131 @@ public class Csvreader {
         }   
     }
 
-    public Boolean CheckLoginDetails(String ID, String Pass){
+    public Boolean CheckLoginDetails(String ID, String Pass, String Usertype){
         for (int i = 0; i < UsersInfo.size(); i++) {
             String[] items = UsersInfo.get(i).split(",");           // split a line by comma
             String id = items[0];                                  // items[0] is id, items[1] is password, items[2] is usertype,
-            String pass = items[1];                               //  items[3] is username , items[4] is 1st vac status, items[5] is 2nd vac status 
+            String pass = items[1];   
+            String usertype = items[2];                            //  items[3] is username , items[4] is 1st vac status, items[5] is 2nd vac status 
             if(ID.equals(id)){
                 if(Pass.equals(pass)){
-                    System.out.println("Success !");
-                    UserLocatedInLine = i;
-                    return true;
+                    if(Usertype.equals(usertype)){
+                        UserLocatedInLine = i;
+                        return true;
+                    }
                 }
             }
         }
-        System.out.println("Failed !");
         return false;
     }
 
-    public String GetUserData(int index){
+    public void addUser( String Pass, String Usertype, String Username, Boolean FstVac, Boolean ScndVac, String Phone){    // adds new user
+        int Last_ID = Integer.parseInt(GetUserData(0,UsersInfo.size() - 1));  
+        ++Last_ID; // get the last ID in csv and increaments it to next ID
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("users.csv", true));
+            writer.append("\n" + Last_ID + "," + Pass + "," + Usertype + "," + Username + "," + FstVac + "," + ScndVac + ",none,none," + Phone + ",none,");
+            writer.close();
+        } catch (IOException ex) {
+            System.out.println("No file found.");
+        }
+    }
+
+
+    public String GetUserData(int index){     // gets user data by index
         String[] items = UsersInfo.get(UserLocatedInLine).split(","); 
         return  items[index];
     }
+
+    public String GetUserData(int index, int userline){  // finds user by line number and gets user data by index 
+        String[] items = UsersInfo.get(userline).split(","); 
+        return  items[index];
+    }
+
+    public void GetUserInfoByID(String ID){      // gets all user info by ID
+        for (int i = 0; i < UsersInfo.size(); i++) {
+            String[] items = UsersInfo.get(i).split(",");          
+            String id = items[0];                                  
+            if(ID.equals(id)){
+                    System.out.println("User ID: " + ID + ", Name: " + GetUserData(NAME_INDEX,i)  + ", First Vaccine Status: " + GetUserData(FSTSTATUS_INDEX,i) + ", Second Vaccine Status: " + GetUserData(SCNDSTATUS_INDEX,i) 
+                    + ", First Appoinment: " + GetUserData(FIRSTVAC_INDEX,i) + ", Second Appoinment: " + GetUserData(SCNDVAC_INDEX,i) + ", Phone: " + GetUserData(PHONE_INDEX,i));
+                }
+            }
+    }
     
     
-    public List<String> getUserInfo(){
+    public String GetUserDataByID(String ID, int index){   // gets specific user data by ID
+        for (int i = 0; i < UsersInfo.size(); i++) {
+            String[] items = UsersInfo.get(i).split(",");          
+            String id = items[0];                                  
+            if(ID.equals(id)){
+                return items[index];
+            }
+        }
+        return "NOT FOUND";
+    }
+    
+    public void setUserData(String ID, String Data, int index){    // searches for user by ID then sets specific data by index 
+        int LineToBeEdited = 0;
+        String newLine = "";
+        String[] items;
+
+        for (int i = 0; i < UsersInfo.size(); i++) {
+            items = UsersInfo.get(i).split(",");          
+            String id = items[0];                                  
+            if(ID.equals(id)){
+                LineToBeEdited = i;
+                items[index] = Data;
+                for(int x=0; x<items.length; x++){
+                    if(x == 0){
+                        newLine += items[x];
+                    }else{
+                        newLine += "," + items[x];
+                    }
+                }
+            }
+        }
+
+        UsersInfo.remove(LineToBeEdited);
+        UsersInfo.add(LineToBeEdited, newLine);
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("users.csv", false));
+            for(int i=0;i<UsersInfo.size();i++){
+                if(i == 0){
+                    writer.write(UsersInfo.get(i));
+                }else{
+                    writer.write('\n' + UsersInfo.get(i));
+                }
+            }
+            writer.close();
+        } catch (IOException ex) {
+            System.out.println("No file found.");
+        }
+    }
+
+    public List<String> getUserInfo(){    // return list containing all user data
         return UsersInfo;
+    }
+
+    public int getUserLineLocation(){     // return line number where user info located at
+        return UserLocatedInLine;
+    }
+
+    public int ComparenCountField(int index, String Compare){      //  finds and counts user data from speicifed data fields (can be used for Statistic)
+        int c = 0;
+        for (int i = 0; i < UsersInfo.size(); i++) {
+            String[] items = UsersInfo.get(i).split(",");          
+            String Comparedto = items[index];                                  
+            if(Comparedto.equals(Compare) && Comparedto != null){
+                c++;
+            }
+        }
+        return c;
     }
 
 
 }
+
+
 
