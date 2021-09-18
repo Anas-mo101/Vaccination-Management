d2499d9178819b8b4a0d3151c1544f8f3b9e6e35
Anas-mo101 <77382332+Anas-mo101@users.noreply.github.com>
2021-09-18 14:54:07 +0800

    New improved code

--- AllMenus.java
+++ AllMenus.java
@@ -1,9 +1,9 @@
-
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
-public class AllMenus extends Customer {
-    public static void main(String[] args) {
+public class AllMenus{
+
+    AllMenus(){
         RoleMenu();
     }
 
@@ -16,10 +16,10 @@ public class AllMenus extends Customer {
                 System.out.println("|               WELCOME TO JAVA COVID-19 VACCINATION PROGRAM                 |");
                 System.out.println("+============================================================================+");
 
-                System.out.println("Receipient view");
                 System.out.println("1. Receipient");
-                // System.out.println("2. MOH");
-                // System.out.println("3. VC");
+                System.out.println("2. MOH");
+                System.out.println("3. VC");
+                System.out.println("4. Register");
                 System.out.println("0. Exit");
                 System.out.print("Enter a choice: ");
 
@@ -31,14 +31,14 @@ public class AllMenus extends Customer {
                         System.exit(0);
                         break;
                     case 1:
-                        CustomerMenu();
-                        break;
-                    // case 2:
-                    // MOH();
-                    // break;
-                    // case 3:
-                    // VC();
-                    // break;
+                            CustomerMenu();
+                            break;
+                    case 2:
+                            MOH();
+                            break;
+                    case 3:
+                            VC();
+                            break;
                     default:
                         System.out.println("Wrong input! Please enter again: \n");
 
@@ -52,17 +52,16 @@ public class AllMenus extends Customer {
     }
 
     public static void CustomerMenu() {
-
+        Customer customer = new Customer("recipient");
         Scanner input = new Scanner(System.in);
-        boolean signInValue = false;
         int choice;
 
         try {
             do {
                 System.out.println();
-                System.out.println("Customer Menu\n");
-                System.out.println("1.Register");
-                System.out.println("2.Sign-In");
+                System.out.println("\nCustomer Menu");
+                System.out.println("1. View Customer Status");
+                System.out.println("2. View Appointment Date");
                 System.out.println("0. Exit");
                 System.out.print("Enter a choice: ");
 
@@ -73,13 +72,10 @@ public class AllMenus extends Customer {
                         RoleMenu();
                         break;
                     case 1:
-                        AllMenus obj = new AllMenus();
-                        obj.saveCustomertoFile();
+                        customer.ViewCustomerStatus();
                         break;
                     case 2:
-                        AllMenus obj1 = new AllMenus();
-                        obj1.CustomerSignIn();
-                        // signInValue = CustomerSignIn();
+                        customer.ViewAppointmentDate();
                         break;
                     default:
                         System.out.println("Invalid input. Please enter again: ");
@@ -92,4 +88,101 @@ public class AllMenus extends Customer {
             CustomerMenu();
         }
     }
+
+
+    public static void VC() {
+        VaccinationCenter vc = new VaccinationCenter("vcadmin");
+        Scanner input = new Scanner(System.in);
+        int choice;
+
+        try {
+            do {
+                System.out.println();
+                System.out.println(vc.getID());
+                System.out.println(vc.getUsername());
+                System.out.println("\nVaccination Center Menu");
+                System.out.println("1. Print Recipient List");
+                System.out.println("2. Set Appointment Date");
+                System.out.println("3. Vaccine Status");
+                System.out.println("0. Exit");
+                System.out.print("Enter a choice: ");
+
+                choice = input.nextInt();
+
+                switch (choice) {
+                    case 0:
+                        RoleMenu();
+                        break;
+                    case 1:
+                        vc.PrintRecipientList();
+                        break;
+                    case 2:
+                        vc.setAppointmentDate();
+                        break;
+                    case 3:
+                        vc.setVaccineStatus();
+                        break;
+                    default:
+                        System.out.println("Invalid input. Please enter again: ");
+                        break;
+                }
+            } while (choice != 0);
+
+        } catch (InputMismatchException ex) {
+            System.out.println("Enter a number. Please try Again. \n");
+            VC();
+        }
+
+    }
+
+
+    public static void MOH() {
+        Moh moh = new Moh("admin");
+        Scanner input = new Scanner(System.in);
+        int choice;
+
+        try {
+            do {
+                System.out.println();
+                System.out.println("\nMOH Menu");
+                System.out.println("1. Add User");
+                System.out.println("2. Search Recipient");
+                System.out.println("3. Set Vaccine Status");
+                System.out.println("0. Exit");
+                System.out.print("Enter a choice: ");
+
+                choice = input.nextInt();
+
+                switch (choice) {
+                    case 0:
+                        RoleMenu();
+                        break;
+                    case 1:
+                        moh.addUser();
+                        break;
+                    case 2:
+                        moh.searchRecipientData();
+                        break;
+                    case 3:
+                        moh.viewData();
+                        break;
+                    case 4:
+                        moh.viewStatistic();
+                        break;
+                    case 5:
+                        moh.setReceipientVC();
+                        break;
+                    default:
+                        System.out.println("Invalid input. Please enter again: ");
+                        break;
+                }
+            } while (choice != 0);
+
+        } catch (InputMismatchException ex) {
+            System.out.println("Enter a number. Please try Again. \n");
+            MOH();
+        }
+
+    }
+
 }
