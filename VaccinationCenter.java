7d2fb9ca41a7b7bd386408f975bc37db5c04ee57
Anas-mo101 <77382332+Anas-mo101@users.noreply.github.com>
2021-09-18 15:00:00 +0800

    New improved code

--- VaccinationCenter.java
+++ VaccinationCenter.java
@@ -5,19 +5,20 @@ import java.util.List;
 // don't have the save file function
 
 public class VaccinationCenter extends User {
-    
+    Csvreader csv = new Csvreader();
+    private final int CAPACITY_INDEX = 10;
+    private final int FSTSTATUS_INDEX = 4;
+    private final int SCNDSTATUS_INDEX = 5;
+    private final int FSTVACDATE_INDEX = 6;
+    private final int SCNDVACDATE_INDEX = 7;
+    private int CapacityPerHour;
     List<String> userInfo = UsersData.getUserInfo();
 
-    VaccinationCenter() {}
-
-    /*    Not sure this overloaded constructor is needed or not
-
-    VaccinationCenter(String ID, String Username, String Password, int capacityOfVCPerDay) {
-        super(ID, Password);
-        this.capacityOfVCPerDay = capacityOfVCPerDay;
-        //userInfo.add(new user(ID, Username, Password, capacityOfVCPerDay));           
+    VaccinationCenter(String usertype){
+        super(usertype);
+        CapacityPerHour = Integer.parseInt(csv.GetUserDataByID(getID(),CAPACITY_INDEX));
     }
-    */
+
 
     public void PrintRecipientList() {                  // This function is to print the recipient list
         for(int i = 4; i < userInfo.size(); i++) {
@@ -25,102 +26,72 @@ public class VaccinationCenter extends User {
         }
     }
 
-    public void set1stAppointmentDate(String Username, String  date, String VCLocation) {      // set the 1st appoinment date for recipient
-        boolean stop = false;
-        for (int i = 4; i<userInfo.size() && stop==false; i++) {                                            
-            String[] items = userInfo.get(i).split(",");                    
-            String name = items[3];
-            if(getCapacityNow(VCLocation,date) < getCapacity(VCLocation)){       // check the capacity of VC is full or not
-                if(Username.equals(name)){
-                    items[7] = date;
-                    items[5] = "Appointment Made";
-                    System.out.println("An appointment has been made successfuly!!!");
-                    stop = true;                                                 // stop the for loop, if the appointment is made
-                }
-            }
-            else
-                System.out.println("This Vaccination Center has reached the max capacity at this date. Please select another Vaccination Center.");
-                stop = true;                                                      // stop the for loop, if the appointment date reached the max capacity
+    public void setAppointmentDate() {      // set the 1st appoinment date for recipient
+        System.out.println("Enter Recipient ID: ");
+        String ID = input.nextLine();
+
+        System.out.println("Enter either first or second vaccination (1/2): ");
+        int WhichVac = Integer.parseInt(input.nextLine());
+        if(WhichVac == 1){
+            WhichVac = FSTVACDATE_INDEX;
+        }if(WhichVac == 2){
+            WhichVac = SCNDVACDATE_INDEX;
         }
+        
+        System.out.println("Enter Vaccination Date (DD/MM/YYYY): ");
+        String Date = input.nextLine();
+
+        while(true){
+            if(checkCapacity(Date)){
+                break;
+            }else{
+                System.out.println("Max Capacity Reached");
+            }
+        }
+        
+        csv.setUserData(ID,Date,WhichVac);
     }
     
-    public void set2ndAppointmentDate(String Username, String  date, String VCLocation) {    // set the 2nd appoinment date for recipient
-        boolean stop = false;
-        for (int i = 4; i<userInfo.size() && stop==false; i++) {
-            String[] items = userInfo.get(i).split(",");
-            String name = items[3];
-            if(getCapacityNow(VCLocation,date) < getCapacity(VCLocation)){      // check the capacity of VC is full or not
-                if(Username.equals(name)){
-                    items[8] = date;
-                    items[5] = "Completed";
-                    items[6] = "Appointment Made";
-                    System.out.println("An appointment has been made successfuly!!!");
-                    stop = true;                                               // stop the for loop, if the appointment is made
-                }
-            }
-            else
-                System.out.println("This Vaccination Center has reached the max capacity at this date. Please select another Vaccination Center.");
-                stop = true;                                                   // stop the for loop, if the appointment date reached the max capacity
-        }
-    }
 
-    public void set1stVaccineStatus(String Username, String Status) {           // to change status for 1st vaccination
-        for (int i = 4; i<userInfo.size(); i++) {
-            String[] items = userInfo.get(i).split(",");
-            String name = items[3];
-            if(Username.equals(name)){
-                items[5] = Status;               
-                System.out.println("1st appointment status has been updated successfuly!!!");
-            }
-        }
-    }
+    public void setVaccineStatus() {           // to change status for 1st vaccination
+        System.out.println("Enter Recipient ID: ");
+        String ID = input.nextLine();
 
-    public void set2ndVaccineStatus(String Username, String Status) {           // to change status for 2nd vaccination
-        for (int i = 4; i<userInfo.size(); i++) {
-            String[] items = userInfo.get(i).split(",");
-            String name = items[3];
-            if(Username.equals(name)){
-                items[6] = Status;               
-                System.out.println("2nd appointment status has been updated successfuly!!!");
-            }
+        System.out.println("Enter either first or second vaccination (1/2): ");
+        int WhichVac = Integer.parseInt(input.nextLine());
+        if(WhichVac == 1){
+            WhichVac = FSTSTATUS_INDEX;
+        }if(WhichVac == 2){
+            WhichVac = SCNDSTATUS_INDEX;
         }
-    }
-    
-    public int getCapacityNow(String  VCLocation, String date) {                          // to get the capacity of date  
-                                                                                          // (not sure this function works or not)
-        int CapacityNow = 0;
-        for (int i = 4; i<userInfo.size(); i++) {
-            String[] items = userInfo.get(i).split(",");
-            String F_Location = items[7];
-            String F_AppointDate = items [6];
-            String S_Location = items[9];
-            String S_AppointDate = items [8];
-            if(F_Location.equals(VCLocation)) {
-                if(F_AppointDate.equals(date))
-                    CapacityNow++;
-            }else if(S_Location.equals(VCLocation)) {
-                if(S_AppointDate.equals(date))
-                    CapacityNow++;
-            }  
-        }
-        return CapacityNow;
-    }
 
-    public int getCapacity(String VCLocation) {             // Checked, this function worked
-        int capacity=0;
-        for (int i = 2; i<4; i++) {
-            String[] items = userInfo.get(i).split(",");
-            String vclocation = items[3];
-            if(VCLocation.equals(vclocation)){              // get the capacity of VC from the CSV file
-                capacity= Integer.parseInt(items[10]);
-                return capacity;
-            }
-        }
-        return capacity;
+        System.out.println("Set Vaccine Status (true/false): ");
+        String status = input.nextLine();
+        
+        csv.setUserData(ID,status,WhichVac);
     }
 
     
+    public Boolean checkCapacity(String Date) {                          // to check capacity reached or not 
+        int maxCapacity = CapacityPerHour * 24;                    // calculate max capacity through whole day
+        int CurrentCapaicty = csv.ComparenCountField(FSTVACDATE_INDEX, Date) + csv.ComparenCountField(SCNDVACDATE_INDEX, Date);
+        if(CurrentCapaicty < maxCapacity){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
 }
+
+
+
+
+
+
+
+
 
 
 
