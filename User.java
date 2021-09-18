75dab7032fdb7d1447839d750b7a02197850d3a8
Anas-mo101 <77382332+Anas-mo101@users.noreply.github.com>
2021-09-18 14:58:39 +0800

    New improved code

--- User.java
+++ User.java
@@ -1,35 +1,39 @@
 
+import java.util.*;
 
-public class User {
+public class User{
     Csvreader UsersData = new Csvreader();
+    Scanner input = new Scanner(System.in);
     private String ID;
     private String Password;
     private String Username;
     private String UserType;
-    private final int ID_INDEX = 0;
-    private final int PASS_INDEX = 1;
+    private int UserLocatedInLine;
     private final int USERTYPE_INDEX = 2;
-    private final int USERNAME_INDEX = 3;       // items[0] is id, items[1] is password, items[2] is usertype,
-    private final int FIRSTVAC_INDEX = 4;      //  items[3] is username , items[4] is 1st vac status, items[5] is 2nd vac status 
-    private final int SCNDVAC_INDEX = 5;
+    private final int USERNAME_INDEX = 3;       // items[0] is id, items[1] is password, items[2] is usertype
 
-    User() {}
-
-    User(String ID_in, String Pass_in)                
-    {                                             
-        if(login(ID_in, Pass_in)){
-            System.out.println(toString());
-        }
+    User(String usertype){                                             
+        login(usertype);
     }
 
-    public Boolean login(String ID_in, String Pass_in){
-        if(UsersData.CheckLoginDetails(ID_in, Pass_in)){
-            ID = ID_in;
-            UserType = UsersData.GetUserData(USERTYPE_INDEX); 
-            Username = UsersData.GetUserData(USERNAME_INDEX); 
-            return true;
-        }else{
-            return false;
+    public void login(String usertype){
+        
+        while(true){
+            System.out.print("ID: ");
+            String ID_in = input.nextLine();
+            System.out.print("Password: ");
+            String Pass_in = input.nextLine();
+
+            if(UsersData.CheckLoginDetails(ID_in, Pass_in, usertype)){
+                ID = ID_in;
+                UserType = UsersData.GetUserData(USERTYPE_INDEX); 
+                Username = UsersData.GetUserData(USERNAME_INDEX); 
+                UserLocatedInLine = UsersData.getUserLineLocation();
+                System.out.println("Loged in");
+                break;
+            }else{
+                System.out.println("Invalid user ID or Passwod");
+            }
         }
     }
     
@@ -37,13 +41,16 @@ public class User {
 
     }
 
-    public String toString(){
-        if(UserType.equals("recipient")){
-            return "ID: " + ID + ", Username: " + Username + ", User type: " + UserType +
-                        ", First Dose: " + UsersData.GetUserData(FIRSTVAC_INDEX) + 
-                            ", Second Dose: " + UsersData.GetUserData(SCNDVAC_INDEX);
-        }else{
-            return "ID: " + ID + ", Username: " + Username + ", User type: " + UserType;
-        }
+    public String getID() {
+        return ID;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public int getUserLine(){
+        return UserLocatedInLine;
     }
 }
+
