8dd79a6bc161e4e4022090ca42f37aefe190f962
Anas-mo101 <77382332+Anas-mo101@users.noreply.github.com>
2021-09-19 23:08:31 +0800

    updated all user menus

--- AllMenus.java
+++ AllMenus.java
@@ -59,48 +59,43 @@ public class AllMenus {
     public static void CustomerMenu() {
         Customer customer = new Customer("recipient");
         Scanner input = new Scanner(System.in);
-        int choice;
+        String choice;
 
-        try {
-            do {
-                System.out.println();
-                System.out.println("------------------------------\n  CUSTOMER MENU \n------------------------------");
+        do {
+            System.out.println();
+            System.out.println("------------------------------\n  CUSTOMER MENU \n------------------------------");
 
-                System.out.println("1. View Customer Status");
-                System.out.println("2. View Appointment Date");
-                System.out.println("0. Exit");
-                System.out.print("Enter a choice: ");
+            System.out.println("1. View Customer Status");
+            System.out.println("2. View Appointment Date");
+            System.out.println("0. Exit");
+            System.out.print("Enter a choice: ");
 
-                choice = input.nextInt();
+            choice = input.nextLine();
 
-                switch (choice) {
-                    case 0:
-                        RoleMenu();
-                        break;
-                    case 1:
-                        customer.ViewCustomerStatus();
-                        break;
-                    case 2:
-                        customer.ViewAppointmentDate();
-                        break;
-                    default:
-                        System.out.println("Invalid input. Please enter again: ");
-                        break;
-                }
+            switch (choice) {
+                case "0":
+                    RoleMenu();
+                    break;
+                case "1":
+                    customer.ViewCustomerStatus();
+                    break;
+                case "2":
+                    customer.ViewAppointmentDate();
+                    break;
+                default:
+                    System.out.println("Invalid input. Please enter again: ");
+                    break;
+            }
 
-            } while (choice != 0);
-        } catch (InputMismatchException ex) {
-            System.out.println("Enter a number. Please try Again. \n");
-            CustomerMenu();
-        }
+        } while (!choice.equals("0"));
     }
 
     public static void VC() {
         VaccinationCenter vc = new VaccinationCenter("vcadmin");
         Scanner input = new Scanner(System.in);
-        int choice;
+        String choice;
 
-        try {
+
             do {
                 System.out.println();
                 System.out.println(vc.getID());
@@ -113,83 +108,75 @@ public class AllMenus {
                 System.out.println("0. Exit");
                 System.out.print("Enter a choice: ");
 
-                choice = input.nextInt();
+                choice = input.nextLine();
 
                 switch (choice) {
-                    case 0:
+                    case "0":
                         RoleMenu();
                         break;
-                    case 1:
+                    case "1":
                         vc.PrintRecipientList();
                         break;
-                    case 2:
+                    case "2":
                         vc.setAppointmentDate();
                         break;
-                    case 3:
+                    case "3":
                         vc.setVaccineStatus();
                         break;
                     default:
                         System.out.println("Invalid input. Please enter again: ");
                         break;
                 }
-            } while (choice != 0);
-
-        } catch (InputMismatchException ex) {
-            System.out.println("Enter a number. Please try Again. \n");
-            VC();
-        }
-
+            } while (!choice.equals("0"));
     }
 
     public static void MOH() {
         Moh moh = new Moh("admin");
         Scanner input = new Scanner(System.in);
-        int choice;
+        String choice;
 
-        try {
             do {
                 System.out.println();
                 System.out.println("------------------------------\n  MOH MENU \n------------------------------");
 
                 System.out.println("1. Add User");
                 System.out.println("2. Search Recipient");
-                System.out.println("3. Set Vaccine Status");
+                System.out.println("3. View Recipient List");
+                System.out.println("4. Set Vaccine Status");
                 System.out.println("0. Exit");
                 System.out.print("Enter a choice: ");
 
-                choice = input.nextInt();
+                choice = input.nextLine();
 
                 switch (choice) {
-                    case 0:
+                    case "0":
                         RoleMenu();
                         break;
-                    case 1:
+                    case "1":
                         moh.addUser();
                         break;
-                    case 2:
+                    case "2":
                         moh.searchRecipientData();
                         break;
-                    case 3:
+                    case "3":
                         moh.viewData();
                         break;
-                    case 4:
+                    case "4":
                         moh.viewStatistic();
                         break;
-                    case 5:
-                        moh.distributeVaccine();
+                    case "5":
+                        moh.distributeVaccine(); 
                         break;
                     default:
                         System.out.println("Invalid input. Please enter again: ");
                         break;
                 }
-            } while (choice != 0);
-
-        } catch (InputMismatchException ex) {
-            System.out.println("Enter a number. Please try Again. \n");
-            MOH();
-        }
-
+            } while (!choice.equals("0"));
     }
+    
+    
+    
+    
     public static String center(String text, int len){
         String out = String.format("%"+len+"s%s%"+len+"s", "",text,"");
         float mid = (out.length()/2);
