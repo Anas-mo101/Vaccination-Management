dda0394bd80454ba7a4740d9220fa08d0c3ca59a
Anas-mo101 <77382332+Anas-mo101@users.noreply.github.com>
2021-09-19 18:12:15 +0800

    fixed error in Login()
    ID was not assigned when logged in
    removed duplicated and unused lines

--- User.java
+++ User.java
@@ -8,6 +8,7 @@ public class User {
     private String Username;
     private String UserType;
     private int UserLocatedInLine;
+    private final int ID_INDEX = 0;
     private final int USERTYPE_INDEX = 2;
     private final int USERNAME_INDEX = 3; // items[0] is id, items[1] is password, items[2] is usertype
     private final int PASSWORD_INDEX = 4;
@@ -23,19 +24,16 @@ public class User {
         System.out.println("---------------\n  LOGIN \n---------------");
         while (true) {
             System.out.print("Name: ");
-            // String ID_in = input.nextLine();
             String Username_in = input.nextLine();
             System.out.print("Password: ");
             String Pass_in = input.nextLine();
 
             if (UsersData.CheckLoginDetails(Username_in, Pass_in, usertype)) {
-                Username = Username_in;
-                Password = Pass_in;
+                ID = UsersData.GetUserData(ID_INDEX);
                 UserType = UsersData.GetUserData(USERTYPE_INDEX);
                 Username = UsersData.GetUserData(USERNAME_INDEX);
-                UserLocatedInLine = UsersData.getUserLineLocation();
                 Password = UsersData.GetUserData(PASSWORD_INDEX);
-                UserLocatedInLine = UsersData.getUserLineLocation();
+                // UserLocatedInLine = UsersData.getUserLineLocation();
                 System.out.println("SUCCESSFULLY LOGIN!!");
                 break;
             } else {
