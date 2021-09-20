2ffabec88882206e31b9024c91b46fbc1789fc85
ChinWeiSong <78065106+ChinWeiSong@users.noreply.github.com>
2021-09-20 20:19:11 +0800

    Made some changes to the viewStatic()

--- Moh.java
+++ Moh.java
@@ -80,11 +80,11 @@ public class Moh extends User{
 
     public void viewStatistic() { 
         System.out.println("\tVaccination Stats:\n");
-        System.out.println("\tReceive 1st Dose Date of vaccination! \n\t\t ==> "+(204-csv.ComparenCountField(6, "none")));
-        System.out.println("\tComplete 1st Dose of Vaccination! \n\t\t ==> "+ csv.ComparenCountField(4, "made"));
-        System.out.println("\tReceive 2nd Dose Date of vaccination! \n\t\t ==> "+ (204-csv.ComparenCountField(7, "none")));
-        System.out.println("\tComplete 2nd Dose of Vaccination! \n\t\t ==> "+ csv.ComparenCountField(5, "made"));
-        System.out.println("\tComplete Both Dose of Vaccination! \n\t\t ==> "+ csv.ComparenCountField(5, "made"));
+        System.out.println("\tReceive 1st Dose Date of vaccination! \n\t\t ==> "+ (csv.ComparenCountField(4, "Appointment made")) );
+        System.out.println("\tComplete 1st Dose of Vaccination! \n\t\t ==> "+ csv.ComparenCountField(4, "Done"));
+        System.out.println("\tReceive 2nd Dose Date of vaccination! \n\t\t ==> "+ (csv.ComparenCountField(5, "Appointment made")));
+        System.out.println("\tComplete 2nd Dose of Vaccination! \n\t\t ==> "+ csv.ComparenCountField(5, "Done"));
+        System.out.println("\tComplete Both Dose of Vaccination! \n\t\t ==> "+ (csv.ComparenCountField(4, "Done")+ csv.ComparenCountField(5, "Done")));
         
     }
 
