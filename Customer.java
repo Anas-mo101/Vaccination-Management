fda640f69a803073a80231e2a44c97bbbbe0c654
frhsof <90459921+frhsof@users.noreply.github.com>
2021-09-27 15:25:40 +0800

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 import java.util.*;
 
+/**
+ * A Customer class representing the details of the customer. 1) custName is the
+ * customer's name, 2) custPhone is the customer's phone number, 3) password is
+ * the customer's password, 4) custStatus is the customer's status and vaccine
+ * appointment
+ *
+ * @author Sharifah Farah Sofea
+ */
+
 public class Customer extends User {
     Csvreader csv = new Csvreader(); // to handle all csv actions
     private final int NAME_INDEX = 3;
@@ -18,6 +27,10 @@ public class Customer extends User {
         super(i);
     }
 
+    /**
+     * Saves the Customer's name, phone number, password, status and vaccine
+     * appointment into the "users.csv" file once customer successfully registered
+     */
     public void saveCustomertoFile() { // recipient regiesteration
         Scanner input = new Scanner(System.in);
 
@@ -48,7 +61,13 @@ public class Customer extends User {
         csv.addUser(password, "recipient", custName, "Pending", "Pending", custPhone, "none");
         AllMenus.RoleMenu();
     }
-
+    
+    /**
+     * Display Customer details like name, phone number and status of their first
+     * dose and second dose vaccine. 1) Pending means still waiting for appointment
+     * date, 2) 1st dose complete means the customer has received the first dose, 3)
+     * 2nd dose complete means the customer has received the second dose.
+     */
     public void ViewCustomerStatus() {
         System.out.println("Name: " + csv.GetUserData(NAME_INDEX, getUserLine()));
         System.out.println("Phone number: " + csv.GetUserData(PHONE_INDEX, getUserLine()));
@@ -56,6 +75,10 @@ public class Customer extends User {
         System.out.println("Second Vaccine Status -> " + csv.GetUserData(SCNDSTATUS_INDEX, getUserLine()));
     }
 
+    /**
+     * Display Customer details like name, phone number and appointment date of
+     * their vaccine.
+     */
     public void ViewAppointmentDate() {
         System.out.println("Name: " + csv.GetUserData(NAME_INDEX, getUserLine()));
         System.out.println("Phone number: " + csv.GetUserData(PHONE_INDEX, getUserLine()));
@@ -63,6 +86,14 @@ public class Customer extends User {
         System.out.println("Second Vaccine Appointment -> " + csv.GetUserData(SCNDVAC_INDEX, getUserLine()));
     }
     
+    /**
+     * Checking a valid phone number of customer whether it meet the requirement
+     * such as first character is 0, second character is 1 and the length of phone
+     * number is 10 digit.
+     * 
+     * @param phone number of customer in String
+     * @return boolean true or false based on requirements
+     */
     public boolean valPhone(String custPhone) {
         return custPhone.charAt(0) == '0' && custPhone.charAt(1) == '1' && custPhone.length() == 10;
 
