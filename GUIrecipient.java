6d7a246c34714f018205c99ea316f7446ecbdaa0
frhsof <90459921+frhsof@users.noreply.github.com>
2021-11-23 02:04:11 +0800

    Add files via upload

---
+++ GUIrecipient.java
@@ -0,0 +1,111 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
+
+public class GUIrecipient extends Application {
+    Csvreader csv = new Csvreader();
+    private final int NAME_INDEX = 3;
+    private final int FSTSTATUS_INDEX = 4;
+    private final int SCNDSTATUS_INDEX = 5;
+    private final int FIRSTVAC_INDEX = 6;
+    private final int SCNDVAC_INDEX = 7;
+    private final int PHONE_INDEX = 8;
+    private final int AGE_INDEX = 11;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage mainStage) throws Exception {
+        mainStage.setTitle("RECIPIENT MENU");
+
+        Button buttonStatus = new Button();
+        buttonStatus.setText("View Recipient Status");
+        buttonStatus.setOnAction(e -> {
+            ViewStatus();
+        });
+
+        Button buttonExit = new Button();
+        buttonExit.setText("Exit");
+        buttonExit.setOnAction(e -> {
+            mainStage.close();
+        });
+
+        HBox hBoxMenu = new HBox();
+        hBoxMenu.setPrefWidth(200);
+        hBoxMenu.setAlignment(Pos.TOP_CENTER);
+        hBoxMenu.setSpacing(30);
+        hBoxMenu.setPadding(new Insets(90, 5, 5, 5));
+        hBoxMenu.getChildren().addAll(buttonStatus, buttonExit);
+
+        Scene scene = new Scene(hBoxMenu, 800, 250);
+        mainStage.setScene(scene);
+        mainStage.show();
+
+    }
+
+    public void ViewStatus() {
+        Csvreader csv = new Csvreader();
+        Stage stage = new Stage();
+        Vaccine vac = new Vaccine();
+
+        stage.setTitle("VIEW RECIPIENT STATUS");
+
+        Label tfName = new Label("\tName \t ==>  " + csv.GetUserData(NAME_INDEX, getUserLine()));
+        Label tfAge = new Label("\tAge \t ==>  " + csv.GetUserData(AGE_INDEX, getUserLine()));
+        Label tfPhone = new Label("\tPhone number \t ==>  " + csv.GetUserData(PHONE_INDEX, getUserLine()));
+        Label tfVac1 = new Label("\t1st Vaccine Status \t ==>  " + csv.GetUserData(FSTSTATUS_INDEX, getUserLine())
+                + " - " + csv.GetUserData(FIRSTVAC_INDEX, getUserLine()));
+        Label tfBatch1 = new Label("\tBatch number \t ==>  " + vac.addVacBatchNO());
+        Label tfVac2 = new Label("\t2nd Vaccine Status \t ==>  " + csv.GetUserData(SCNDSTATUS_INDEX, getUserLine())
+                + " - " + csv.GetUserData(SCNDVAC_INDEX, getUserLine()));
+        Label tfBatch2 = new Label("\tBatch number \t ==>  " + vac.addVacBatchNO());
+
+        VBox vBoxMenu = new VBox();
+        vBoxMenu.setPrefWidth(200);
+        vBoxMenu.setAlignment(Pos.TOP_CENTER);
+        vBoxMenu.setSpacing(30);
+        vBoxMenu.setPadding(new Insets(10, 5, 5, 5));
+        vBoxMenu.getChildren().addAll(tfName, tfAge, tfPhone, tfVac1, tfBatch1, tfVac2, tfBatch2);
+
+        Scene scene = new Scene(vBoxMenu, 600, 350);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private int getUserLine() {
+
+        String currentPath = System.getProperty("user.dir");
+        String FieldDelimiter = ",";
+
+        BufferedReader br;
+        int counter = 0;
+
+        try {
+            br = new BufferedReader(new FileReader(currentPath + "/users.csv"));
+
+            String line;
+            while ((line = br.readLine()) != null)
+                counter++;
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("ERROR :" + ex.getMessage());
+        } catch (IOException ex) {
+
+            System.out.println("ERROR :" + ex.getMessage());
+        }
+        return counter - 1;
+    }
+}
