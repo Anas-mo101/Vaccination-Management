58b1854798fb2adf9ba2cefa513ae8912e8d3ad9
frhsof <90459921+frhsof@users.noreply.github.com>
2021-11-25 17:53:24 +0800

    Update MainMenuMoh.java

--- MainMenuMoh.java
+++ MainMenuMoh.java
@@ -28,8 +28,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.geometry.Insets;
 
-
-public class MainMenuMoh extends Application {
+public class MainMenuMoh {
     Csvreader csv = new Csvreader();
     private final int ID_INDEX = 0;
     private final int USERTYPE_INDEX = 2;
@@ -39,71 +38,72 @@ public class MainMenuMoh extends Application {
     private final TableView<RecipientData> dataTable = new TableView<>();
     private final ArrayList<RecipientData> recipientPosition = new ArrayList<>();
 
-    public static void main (String[] args){
-        launch(args);
-    }
+    Stage mainStage = new Stage();
 
-    @Override
-    public void start(Stage mainStage) throws Exception {
+    MainMenuMoh(String[] data) {
+
+        // @Override
+        // public void start(Stage mainStage) throws Exception {
         mainStage.setTitle("MOH MENU");
 
         Button buttonRecipient = new Button();
         buttonRecipient.setText("Add Recipient");
-        buttonRecipient.setOnAction(e->{
+        buttonRecipient.setOnAction(e -> {
             addRecipent();
         });
 
         Button buttonVC = new Button();
         buttonVC.setText("Add VC");
-        buttonVC.setOnAction(e->{
+        buttonVC.setOnAction(e -> {
             addVC();
         });
-        
+
         Button buttonRecipientList = new Button();
         buttonRecipientList.setText("View Recipient List");
-        buttonRecipientList.setOnAction(e->{
+        buttonRecipientList.setOnAction(e -> {
             readCSV();
             viewRecipientList();
         });
         Button buttonStatistic = new Button();
         buttonStatistic.setText("View Vacination Statistic");
-        buttonStatistic.setOnAction(e->{
+        buttonStatistic.setOnAction(e -> {
             vacinationStatistic();
         });
 
         Button distributeVaccineButton = new Button();
         distributeVaccineButton.setText("Distribute Vaccine");
-        distributeVaccineButton.setOnAction(e->{
+        distributeVaccineButton.setOnAction(e -> {
             distributeVaccine();
         });
 
         Button buttonExit = new Button();
         buttonExit.setText("Exit");
-        buttonExit.setOnAction(e->{
-            mainStage.close();});
+        buttonExit.setOnAction(e -> {
+            mainStage.close();
+        });
 
         HBox hBoxMenu = new HBox();
         hBoxMenu.setPrefWidth(200);
         hBoxMenu.setAlignment(Pos.TOP_CENTER);
         hBoxMenu.setSpacing(30);
         hBoxMenu.setPadding(new Insets(90, 5, 5, 5));
-        hBoxMenu.getChildren().addAll(buttonRecipient,buttonVC, buttonRecipientList, buttonStatistic, distributeVaccineButton, buttonExit);
+        hBoxMenu.getChildren().addAll(buttonRecipient, buttonVC, buttonRecipientList, buttonStatistic,
+                distributeVaccineButton, buttonExit);
 
-        Scene scene = new Scene (hBoxMenu,800,250);
+        Scene scene = new Scene(hBoxMenu, 800, 250);
         mainStage.setScene(scene);
         mainStage.show();
 
-
     }
 
-////////////////////// ADD RECIPIENT //////////////////////////////
+    ////////////////////// ADD RECIPIENT //////////////////////////////
     public void addRecipent() {
         Stage stage = new Stage();
         stage.setTitle("ADD USER");
 
-        Label name = new Label("Enter name:  "); 
+        Label name = new Label("Enter name:  ");
         Label phone = new Label("Enter your phone number: ");
-        Label age = new Label ("Enter your Age: ");
+        Label age = new Label("Enter your Age: ");
         Label password = new Label("Enter a password: ");
 
         TextField nameField = new TextField();
@@ -111,22 +111,21 @@ public class MainMenuMoh extends Application {
         TextField passField = new TextField();
         TextField ageField = new TextField();
 
-
         Button submit = new Button();
         submit.setText("Submit");
-        submit.setOnAction(e->{
-            csv.addUser(passField.getText(), "recipient", nameField.getText(), "Pending", "Pending", phoneField.getText(), "none", ageField.getText());
+        submit.setOnAction(e -> {
+            csv.addUser(passField.getText(), "recipient", nameField.getText(), "Pending", "Pending",
+                    phoneField.getText(), "none", ageField.getText());
             apearWindow.display("Succesfull", "Succesfully Updated!");
             stage.close();
         });
-        
 
         HBox insertName = new HBox();
         insertName.setPrefWidth(200);
         insertName.setAlignment(Pos.TOP_CENTER);
         insertName.setSpacing(30);
         insertName.setPadding(new Insets(5, 5, 5, 2));
-        insertName.getChildren().addAll(name ,nameField);
+        insertName.getChildren().addAll(name, nameField);
 
         HBox insertPhone = new HBox();
         insertPhone.setPrefWidth(200);
@@ -147,21 +146,21 @@ public class MainMenuMoh extends Application {
         insertAge.setAlignment(Pos.TOP_CENTER);
         insertAge.setSpacing(30);
         insertAge.setPadding(new Insets(5, 5, 5, 8));
-        insertAge.getChildren().addAll(age,ageField);
-        
+        insertAge.getChildren().addAll(age, ageField);
 
         VBox vBoxMenu = new VBox();
         vBoxMenu.setPrefWidth(200);
         vBoxMenu.setAlignment(Pos.TOP_CENTER);
         vBoxMenu.setSpacing(30);
         vBoxMenu.setPadding(new Insets(10, 5, 5, 5));
-        vBoxMenu.getChildren().addAll(insertName,insertAge,insertPhone,insertPass,submit);
+        vBoxMenu.getChildren().addAll(insertName, insertAge, insertPhone, insertPass, submit);
 
-        Scene scene = new Scene (vBoxMenu,550,350);
+        Scene scene = new Scene(vBoxMenu, 550, 350);
         stage.setScene(scene);
         stage.show();
     }
-///////////////// ADD VC ////////////////////
+
+    ///////////////// ADD VC ////////////////////
     public void addVC() {
         Stage stage = new Stage();
         stage.setTitle("ADD USER");
@@ -169,30 +168,28 @@ public class MainMenuMoh extends Application {
         Label name = new Label("Enter name:  ");
         Label phone = new Label("Enter your phone number: ");
         Label password = new Label("Enter a password: ");
-        Label capa = new Label ("Enter a Capacity per day:");
+        Label capa = new Label("Enter a Capacity per day:");
 
         TextField nameField = new TextField();
         TextField phoneField = new TextField();
         TextField passField = new TextField();
         TextField capaField = new TextField();
 
-
-
         Button submit = new Button();
         submit.setText("Submit");
-        submit.setOnAction(e->{
-            csv.addUser(passField.getText(), "vcadmin", nameField.getText(), "none", "none", phoneField.getText(), capaField.getText(), "none");
+        submit.setOnAction(e -> {
+            csv.addUser(passField.getText(), "vcadmin", nameField.getText(), "none", "none", phoneField.getText(),
+                    capaField.getText(), "none");
             apearWindow.display("Succesfull", "Succesfully Updated!");
             stage.close();
         });
-        
 
         HBox insertName = new HBox();
         insertName.setPrefWidth(200);
         insertName.setAlignment(Pos.TOP_CENTER);
         insertName.setSpacing(30);
         insertName.setPadding(new Insets(5, 5, 5, 5));
-        insertName.getChildren().addAll(name ,nameField);
+        insertName.getChildren().addAll(name, nameField);
 
         HBox insertPhone = new HBox();
         insertPhone.setPrefWidth(200);
@@ -207,64 +204,63 @@ public class MainMenuMoh extends Application {
         insertPass.setSpacing(30);
         insertPass.setPadding(new Insets(5, 5, 5, 20));
         insertPass.getChildren().addAll(password, passField);
-     
+
         HBox insertCapa = new HBox();
         insertCapa.setPrefWidth(200);
         insertCapa.setAlignment(Pos.TOP_CENTER);
         insertCapa.setSpacing(30);
         insertCapa.setPadding(new Insets(5, 5, 5, 50));
-        insertCapa.getChildren().addAll(capa,capaField);
+        insertCapa.getChildren().addAll(capa, capaField);
 
         VBox vBoxMenu = new VBox();
         vBoxMenu.setPrefWidth(200);
         vBoxMenu.setAlignment(Pos.TOP_CENTER);
         vBoxMenu.setSpacing(30);
         vBoxMenu.setPadding(new Insets(10, 5, 5, 5));
-        vBoxMenu.getChildren().addAll(insertName,insertPhone,insertPass,insertCapa,submit);
+        vBoxMenu.getChildren().addAll(insertName, insertPhone, insertPass, insertCapa, submit);
 
-        Scene scene = new Scene (vBoxMenu,550,350);
+        Scene scene = new Scene(vBoxMenu, 550, 350);
         stage.setScene(scene);
         stage.show();
     }
-////////////// DISTRIBUTE VACCINE //////////////////////
-    public void distributeVaccine(){
+
+    ////////////// DISTRIBUTE VACCINE //////////////////////
+    public void distributeVaccine() {
         Stage stage = new Stage();
         stage.setTitle("Distribute Vaccine");
 
         Label idFrom = new Label("Enter User ID from");
         Label idTo = new Label("Enter User ID to");
-        Label assignVC = new Label ("Enter Assigned Vaccination center:");
+        Label assignVC = new Label("Enter Assigned Vaccination center:");
 
         TextField idFromField = new TextField();
         TextField idToField = new TextField();
         TextField assignVCField = new TextField();
 
-
-
         Button submit = new Button();
-            submit.setText("Submit");
-            submit.setOnAction(e->{
-                try{
-                    int idFrom_intForm = Integer.parseInt(idFromField.getText());
-                    int idTo_intTo = Integer.parseInt(idToField.getText());
-                    if(idFrom_intForm <5 || idTo_intTo >300 || idFrom_intForm > idTo_intTo ) //change sepcific
-                        throw new Exception("ID out of Index");
-                    csv.GetUserDataByUsername(assignVCField.getText(), USERTYPE_INDEX).equals("vcadmin");
-                    csv.setMultipleUserData(idFromField.getText(),idToField.getText(),assignVCField.getText(),VCASSINGED_INDEX);
-                    System.out.println("succes");
-                }
-                catch(Exception ex){
-                    apearWindow.display("Error!", "close" + ex.getMessage());
-                    System.out.println("fail");
-                }
-            });
-        
+        submit.setText("Submit");
+        submit.setOnAction(e -> {
+            try {
+                int idFrom_intForm = Integer.parseInt(idFromField.getText());
+                int idTo_intTo = Integer.parseInt(idToField.getText());
+                if (idFrom_intForm < 5 || idTo_intTo > 300 || idFrom_intForm > idTo_intTo) // change sepcific
+                    throw new Exception("ID out of Index");
+                csv.GetUserDataByUsername(assignVCField.getText(), USERTYPE_INDEX).equals("vcadmin");
+                csv.setMultipleUserData(idFromField.getText(), idToField.getText(), assignVCField.getText(),
+                        VCASSINGED_INDEX);
+                System.out.println("succes");
+            } catch (Exception ex) {
+                apearWindow.display("Error!", "close" + ex.getMessage());
+                System.out.println("fail");
+            }
+        });
+
         HBox insertFrom = new HBox();
         insertFrom.setPrefWidth(200);
         insertFrom.setAlignment(Pos.TOP_CENTER);
         insertFrom.setSpacing(30);
         insertFrom.setPadding(new Insets(5, 5, 5, 20));
-        insertFrom.getChildren().addAll(idFrom,idFromField);
+        insertFrom.getChildren().addAll(idFrom, idFromField);
 
         HBox insertTo = new HBox();
         insertTo.setPrefWidth(200);
@@ -285,50 +281,58 @@ public class MainMenuMoh extends Application {
         vBoxMenu.setAlignment(Pos.TOP_CENTER);
         vBoxMenu.setSpacing(30);
         vBoxMenu.setPadding(new Insets(10, 5, 5, 5));
-        vBoxMenu.getChildren().addAll(insertFrom,insertTo,insertVC,submit);
+        vBoxMenu.getChildren().addAll(insertFrom, insertTo, insertVC, submit);
 
-        Scene scene = new Scene (vBoxMenu,600,300);
+        Scene scene = new Scene(vBoxMenu, 600, 300);
         stage.setScene(scene);
         stage.show();
-    }   
-////////////// VACCINATION STATISTIC /////////////////////
-    public void vacinationStatistic(){
+    }
+
+    ////////////// VACCINATION STATISTIC /////////////////////
+    public void vacinationStatistic() {
         Stage stage = new Stage();
         stage.setTitle("Vacination Statistic");
 
-        Label receiveOneDose = new Label("\tReceive 1st Dose Date of vaccination! \n\t\t ==> "+ (csv.ComparenCountField(FSTSTATUS_INDEX, "Appointment made")));
-        Label completedOneDose = new Label("\tComplete 1st Dose of Vaccination! \n\t\t ==> "+ csv.ComparenCountField(FSTSTATUS_INDEX, "Done"));
-        Label receiveTwoDose = new Label ("\tReceive 2nd Dose Date of vaccination! \n\t\t ==> "+ (csv.ComparenCountField(SCNDSTATUS_INDEX, "Appointment made")));
-        Label completedTwoDose = new Label("\tComplete 2nd Dose of Vaccination! \n\t\t ==> "+ csv.ComparenCountField(SCNDSTATUS_INDEX, "Done"));
-        Label completedBothDose = new Label("\tComplete Both Dose of Vaccination! \n\t\t ==> "+ (csv.ComparenCountField(FSTSTATUS_INDEX, "Done")+ csv.ComparenCountField(SCNDSTATUS_INDEX, "Done")));
+        Label receiveOneDose = new Label("\tReceive 1st Dose Date of vaccination! \n\t\t ==> "
+                + (csv.ComparenCountField(FSTSTATUS_INDEX, "Appointment made")));
+        Label completedOneDose = new Label(
+                "\tComplete 1st Dose of Vaccination! \n\t\t ==> " + csv.ComparenCountField(FSTSTATUS_INDEX, "Done"));
+        Label receiveTwoDose = new Label("\tReceive 2nd Dose Date of vaccination! \n\t\t ==> "
+                + (csv.ComparenCountField(SCNDSTATUS_INDEX, "Appointment made")));
+        Label completedTwoDose = new Label(
+                "\tComplete 2nd Dose of Vaccination! \n\t\t ==> " + csv.ComparenCountField(SCNDSTATUS_INDEX, "Done"));
+        Label completedBothDose = new Label("\tComplete Both Dose of Vaccination! \n\t\t ==> "
+                + (csv.ComparenCountField(FSTSTATUS_INDEX, "Done") + csv.ComparenCountField(SCNDSTATUS_INDEX, "Done")));
 
         VBox vBoxMenu = new VBox();
         vBoxMenu.setPrefWidth(200);
         vBoxMenu.setAlignment(Pos.TOP_CENTER);
         vBoxMenu.setSpacing(30);
         vBoxMenu.setPadding(new Insets(10, 5, 5, 5));
-        vBoxMenu.getChildren().addAll(receiveOneDose,completedOneDose,receiveTwoDose,completedTwoDose,completedBothDose);
+        vBoxMenu.getChildren().addAll(receiveOneDose, completedOneDose, receiveTwoDose, completedTwoDose,
+                completedBothDose);
 
-        Scene scene = new Scene (vBoxMenu,450,350);
+        Scene scene = new Scene(vBoxMenu, 450, 350);
         stage.setScene(scene);
         stage.show();
     }
-/////////// VIEW RECIPIENT LIST ////////////
-    public void viewRecipientList(){
+
+    /////////// VIEW RECIPIENT LIST ////////////
+    public void viewRecipientList() {
         Stage stage = new Stage();
         stage.setTitle("Vacination Recipient Statistic");
 
-        TableColumn<RecipientData, String> id               = new TableColumn<>("ID");
-        TableColumn<RecipientData, String> pass             = new TableColumn<>("PASS");
-        TableColumn<RecipientData, String> userName         = new TableColumn<>("USERNAME");
-        TableColumn<RecipientData, String> firstVacStatus   = new TableColumn<>("1st Vac Status");
-        TableColumn<RecipientData, String> secondVacStatus  = new TableColumn<>("2nd Vac Status");
-        TableColumn<RecipientData, String> firstVacDate     = new TableColumn<>("1st Vac Date");
-        TableColumn<RecipientData, String> secondVacDate    = new TableColumn<>("2nd Vac Date");
-        TableColumn<RecipientData, String> phone            = new TableColumn<>("Phone");
-        TableColumn<RecipientData, String> assignedVC       = new TableColumn<>("ASSGINED VC");
-        TableColumn<RecipientData, String> age              = new TableColumn<>("Age");
-        
+        TableColumn<RecipientData, String> id = new TableColumn<>("ID");
+        TableColumn<RecipientData, String> pass = new TableColumn<>("PASS");
+        TableColumn<RecipientData, String> userName = new TableColumn<>("USERNAME");
+        TableColumn<RecipientData, String> firstVacStatus = new TableColumn<>("1st Vac Status");
+        TableColumn<RecipientData, String> secondVacStatus = new TableColumn<>("2nd Vac Status");
+        TableColumn<RecipientData, String> firstVacDate = new TableColumn<>("1st Vac Date");
+        TableColumn<RecipientData, String> secondVacDate = new TableColumn<>("2nd Vac Date");
+        TableColumn<RecipientData, String> phone = new TableColumn<>("Phone");
+        TableColumn<RecipientData, String> assignedVC = new TableColumn<>("ASSGINED VC");
+        TableColumn<RecipientData, String> age = new TableColumn<>("Age");
+
         id.setCellValueFactory(new PropertyValueFactory<>("id"));
         pass.setCellValueFactory(new PropertyValueFactory<>("pass"));
         userName.setCellValueFactory(new PropertyValueFactory<>("userName"));
@@ -337,20 +341,21 @@ public class MainMenuMoh extends Application {
         secondVacDate.setCellValueFactory(new PropertyValueFactory<>("secondVacDate"));
         secondVacStatus.setCellValueFactory(new PropertyValueFactory<>("secondVacStatus"));
 
-        dataTable.getColumns().addAll(id,pass,userName,age,phone,assignedVC,firstVacDate,firstVacStatus,secondVacDate,secondVacStatus);
+        dataTable.getColumns().addAll(id, pass, userName, age, phone, assignedVC, firstVacDate, firstVacStatus,
+                secondVacDate, secondVacStatus);
         dataTable.getItems().addAll(recipientPosition);
-    
+
         VBox vBoxMenu = new VBox();
         vBoxMenu.setSpacing(20);
         vBoxMenu.getChildren().add(dataTable);
-                            
-        Scene scene = new Scene (vBoxMenu,800,400);
-        stage.setScene(scene);                    
-        stage.show();             
+
+        Scene scene = new Scene(vBoxMenu, 800, 400);
+        stage.setScene(scene);
+        stage.show();
     }
 
     private void readCSV() {
-        
+
         String currentPath = System.getProperty("user.dir");
         String FieldDelimiter = ",";
 
@@ -362,10 +367,9 @@ public class MainMenuMoh extends Application {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] fields = line.split(FieldDelimiter, -1);
-                if(fields[USERTYPE_INDEX].equals("recipient")) {
-                    RecipientData record = new RecipientData(fields[0], fields[1], fields[3],
-                        fields[11], fields[8], fields[9],fields[6],fields[4],
-                        fields[7],fields[5]);
+                if (fields[USERTYPE_INDEX].equals("recipient")) {
+                    RecipientData record = new RecipientData(fields[0], fields[1], fields[3], fields[11], fields[8],
+                            fields[9], fields[6], fields[4], fields[7], fields[5]);
                     recipientPosition.add(record);
 
                 }
