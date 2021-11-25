91f1dbe1d81e9b081ca0a285b117851bf6514d89
frhsof <90459921+frhsof@users.noreply.github.com>
2021-11-25 23:41:32 +0800

    Update GUIrecipient.java

--- GUIrecipient.java
+++ GUIrecipient.java
@@ -7,8 +7,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.geometry.Insets;
 
 public class GUIrecipient {
@@ -19,8 +22,9 @@ public class GUIrecipient {
     private final int FIRSTVAC_INDEX = 6;
     private final int SCNDVAC_INDEX = 7;
     private final int PHONE_INDEX = 8;
-    private final int VCASSIGNED_INDEX = 9;
     private final int AGE_INDEX = 11;
+    private final int VAC1_INDEX = 13;
+    private final int VAC2_INDEX = 14;
 
     Stage mainStage = new Stage();
 
@@ -28,30 +32,37 @@ public class GUIrecipient {
 
     GUIrecipient(String[] data, String UserName) {
         this.UserName = UserName;
-        // @Override
-        // public void start(Stage mainStage) throws Exception {
+
         mainStage.setTitle("RECIPIENT MENU");
+        Text menuTitle = new Text("Hi dearest customer, " + UserName);
+        Text menuTitle2 = new Text("please choose:");
+        menuTitle.setFont(Font.font("Arial", FontWeight.BOLD, 36));
+        menuTitle.setStroke(Color.BLUE);
+        menuTitle2.setFont(Font.font("Arial", FontWeight.BOLD, 30));
+        menuTitle2.setStroke(Color.BLUE);
 
         Button buttonStatus = new Button();
         buttonStatus.setText("View Recipient Status");
         buttonStatus.setOnAction(e -> {
             ViewStatus(data);
         });
+        buttonStatus.setMinWidth(250);
 
         Button buttonExit = new Button();
         buttonExit.setText("Exit");
         buttonExit.setOnAction(e -> {
             mainStage.close();
         });
+        buttonExit.setMinWidth(175);
 
-        HBox hBoxMenu = new HBox();
-        hBoxMenu.setPrefWidth(200);
-        hBoxMenu.setAlignment(Pos.TOP_CENTER);
-        hBoxMenu.setSpacing(30);
-        hBoxMenu.setPadding(new Insets(90, 5, 5, 5));
-        hBoxMenu.getChildren().addAll(buttonStatus, buttonExit);
+        VBox vBoxMenu = new VBox();
+        vBoxMenu.setPrefWidth(200);
+        vBoxMenu.setAlignment(Pos.TOP_CENTER);
+        vBoxMenu.setSpacing(30);
+        vBoxMenu.setPadding(new Insets(50, 5, 5, 5));
+        vBoxMenu.getChildren().addAll(menuTitle, menuTitle2, buttonStatus, buttonExit);
 
-        Scene scene = new Scene(hBoxMenu, 800, 250);
+        Scene scene = new Scene(vBoxMenu, 675, 300);
         mainStage.setScene(scene);
         mainStage.show();
 
@@ -59,43 +70,29 @@ public class GUIrecipient {
 
     public void ViewStatus(String[] userData) {
         Stage stage = new Stage();
-        Vaccine vac = new Vaccine();
-        // Csvreader csv = new Csvreader();
-
         stage.setTitle("VIEW RECIPIENT STATUS");
+        Text menuTitle3 = new Text("Profile details for " + UserName);
+        menuTitle3.setFont(Font.font("Arial", FontWeight.BOLD, 30));
+        menuTitle3.setStroke(Color.GREEN);
 
-        /*
-         * Label tfName = new Label("\tName \t ==>  " + csv.GetUserData(NAME_INDEX,
-         * getUserLine())); Label tfAge = new Label("\tAge \t ==>  " +
-         * csv.GetUserData(AGE_INDEX, getUserLine())); Label tfPhone = new
-         * Label("\tPhone number \t ==>  " + csv.GetUserData(PHONE_INDEX,
-         * getUserLine())); Label tfVac1 = new Label("\t1st Vaccine Status \t ==>  " +
-         * csv.GetUserData(FSTSTATUS_INDEX, getUserLine()) + " - " +
-         * csv.GetUserData(FIRSTVAC_INDEX, getUserLine())); Label tfBatch1 = new
-         * Label("\tBatch number \t ==>  " + vac.addVacBatchNO()); Label tfVac2 = new
-         * Label("\t2nd Vaccine Status \t ==>  " + csv.GetUserData(SCNDSTATUS_INDEX,
-         * getUserLine()) + " - " + csv.GetUserData(SCNDVAC_INDEX, getUserLine()));
-         * Label tfBatch2 = new Label("\tBatch number \t ==>  " + vac.addVacBatchNO());
-         */
-
-        Label tfName = new Label("\tName \t ==>  " + userData[NAME_INDEX]);
-        Label tfAge = new Label("\tAge \t ==>  " + userData[AGE_INDEX]);
-        Label tfPhone = new Label("\tPhone number \t ==>  " + userData[PHONE_INDEX]);
+        Label tfName = new Label("Name \t  " + userData[NAME_INDEX]);
+        Label tfAge = new Label("Age \t  " + userData[AGE_INDEX]);
+        Label tfPhone = new Label("Phone number \t  " + userData[PHONE_INDEX]);
         Label tfVac1 = new Label(
-                "\t1st Vaccine Status \t ==>  " + userData[FSTSTATUS_INDEX] + " - " + userData[FIRSTVAC_INDEX]);
-        Label tfBatch1 = new Label("\tBatch number \t ==>  " + vac.addVacBatchNO());
+                "1st Vaccine Status \t  " + userData[FSTSTATUS_INDEX] + " - " + userData[FIRSTVAC_INDEX]);
+        Label tfBatch1 = new Label("Batch number \t  " + userData[VAC1_INDEX]);
         Label tfVac2 = new Label(
-                "\t2nd Vaccine Status \t ==>  " + userData[SCNDSTATUS_INDEX] + " - " + userData[SCNDVAC_INDEX]);
-        Label tfBatch2 = new Label("\tBatch number \t ==>  " + vac.addVacBatchNO());
+                "\t2nd Vaccine Status \t   " + userData[SCNDSTATUS_INDEX] + " - " + userData[SCNDVAC_INDEX]);
+        Label tfBatch2 = new Label("Batch number \t   " + userData[VAC2_INDEX]);
 
         VBox vBoxMenu = new VBox();
         vBoxMenu.setPrefWidth(200);
         vBoxMenu.setAlignment(Pos.TOP_CENTER);
         vBoxMenu.setSpacing(30);
         vBoxMenu.setPadding(new Insets(10, 5, 5, 5));
-        vBoxMenu.getChildren().addAll(tfName, tfAge, tfPhone, tfVac1, tfBatch1, tfVac2, tfBatch2);
+        vBoxMenu.getChildren().addAll(menuTitle3, tfName, tfAge, tfPhone, tfVac1, tfBatch1, tfVac2, tfBatch2);
 
-        Scene scene = new Scene(vBoxMenu, 600, 350);
+        Scene scene = new Scene(vBoxMenu, 675, 400);
         stage.setScene(scene);
         stage.show();
     }
@@ -127,3 +124,5 @@ public class GUIrecipient {
         return counter - 1;
     }
 }
+
+
